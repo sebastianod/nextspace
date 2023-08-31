11beba46623c9492c95a4f4a27eb2acfd9644951
sebastianod <sebastianochoad@gmail.com>
2023-08-31 11:33:43 -0500

    Style about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,14 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "About us",
-    description: "This is a static About page with some metadata!",
+  title: "About us",
+  description: "This is a static About page with some metadata!",
 };
 
 export default function StaticAbout() {
   return (
-    <div className="flex flex-col p-5">
-      <h1 className=" mb-5">About</h1>
+    <div className="flex flex-col p-5 items-center">
+      <h1 className=" mb-5 text-2xl">About</h1>
       <p>This is a static About page with some metadata!</p>
     </div>
   );
