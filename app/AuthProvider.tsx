ea0aba4f26db2b7b150a629d0dd335b7795cf97a
sebastianod <sebastianochoad@gmail.com>
2023-09-09 13:29:03 -0500

    Create AuthProvider file wrapper that uses nextAuth's SessionProvider's client side function. This is meant to be used in the whole app, which is a server app.

---
+++ app/AuthProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react"; //access to user session
+
+type Props = {
+    children: React.ReactNode; //very broad, like saying any
+}
+
+//Since sessionProvider is client-side, we created this file to act as a wrapper
+// that can work inside our server app in layout.tsx
+export default function AuthProvider({children} : Props) {
+    return <SessionProvider>{children}</SessionProvider>
+}
