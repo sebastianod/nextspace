7eb4b4b0ead257a8fa3cd04b1ea72aa2c33eb00e
sebastianod <sebastianochoad@gmail.com>
2023-09-09 13:58:58 -0500

    Create signIn and signOut buttons in single function

---
+++ components/buttons.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function SignInButton() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
+
+  const handleSignIn = () => {
+    signIn();
+  }
+
+  if (status === 'loading') {
+    return <>...</>;
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <Link href={`/dashboard`}>
+        <Image
+          src={session.user?.image ?? '/mememan.webp'}
+          width={32}
+          height={32}
+          alt="Your Name"
+        />
+      </Link>
+    );
+  }
+
+  return <button onClick={handleSignIn}>Sign in</button>;
+}
+
+export function SignOutButton() {
+    const handleSignOut = () => {
+        signOut();
+    }
+  return <button onClick={handleSignOut}>Sign out</button>;
+}
