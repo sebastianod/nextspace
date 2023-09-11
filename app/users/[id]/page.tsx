cf662b9554f462293193e210aadcc66ae901a272
sebastianod <sebastianochoad@gmail.com>
2023-09-11 16:17:55 -0500

    Create dynamic metadata for each user's profile through generateMetadata function

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,7 +1,17 @@
 import { prisma } from "@/lib/prisma";
+import { Metadata } from "next"; //to give the type of promise for generateMetadata
 
 interface Props {
   params: { id: string };
+}
+
+//generateMetadata docs: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+//Input, Output explanation:
+//1. Extract the params property from an object of type Props and assign it to a variable called params.
+//2. `: Promise<Metadata>` specifies that this function returns a promise (asynchronous operation) that eventually resolves to an object of type Metadata. 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  return { title: `User profile of ${user?.name}`};
 }
 
 export default async function UserProfile({ params }: Props) {
@@ -15,7 +25,7 @@ export default async function UserProfile({ params }: Props) {
         width={300}
         src={image ?? `/mememan.webp`}
         alt={`${name}'s profile`}
-        className=" mb-3"
+        className=" mb-3 rounded-lg"
       />
       <h3 className=" text-xl">Bio</h3>
       <p>{bio ?? "This is a sample bio."}</p>
