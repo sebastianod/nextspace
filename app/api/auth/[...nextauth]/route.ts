f451a6a48768326e90ebaa7576a0f6a27c8d6ac0
sebastianod <sebastianochoad@gmail.com>
2023-09-10 21:45:30 -0500

    Fix the nextAuthOptions by adding the secret and session options correctly. Change the adapater library to @auth/prisma-adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,21 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import {prisma} from "../../../../lib/prisma"
-import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../../../../lib/prisma";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-  ]
+  ],
 };
 
 const handler = NextAuth(authOptions);
