442f1f2aed3dd777b9e2c2793fe2a0df9d1a477a
sebastianod <sebastianochoad@gmail.com>
2023-09-11 11:37:27 -0500

    Add styles to UserCard and the user's page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,5 +7,5 @@ export default async function Users() {
   const showUsers = users.map((user) => {
     return <UserCard key={user.id} {...user} />;
   });
-  return <div>{showUsers}</div>;
+  return <div className=" grid grid-cols-4 gap-2 p-5">{showUsers}</div>;
 }
--- components/UserCard/UserCard.tsx
+++ components/UserCard/UserCard.tsx
@@ -9,11 +9,17 @@ interface Props {
 
 export default function UserCard({ id, name, age, image }: Props) {
   return (
-    <div>
-      <img alt={`${name}'s photo`} src={image ?? "/mememan.webp"} />
+    <div className=" bg-slate-100 flex flex-col justify-center items-start w-fit p-2">
+      <img
+        alt={`${name}'s photo`}
+        src={image ?? "/mememan.webp"}
+        className=" w-48 mb-3"
+      />
       <div>
-        <h3>
-          <Link href={`/users/${id}`}><span>{name}</span></Link>
+        <h3 className="text-xl mb-3">
+          <Link href={`/users/${id}`}>
+            {name}
+          </Link>
         </h3>
         <p>Age: {age}</p>
       </div>
